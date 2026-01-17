4bf30b001f3fd23f23a3ac9d5f882bab9747f60c
FAYCALDOUIOU <faycal.douiou1999@gmail.com>
2026-01-17 01:24:42 +0100

    re adding the burger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,27 +4,37 @@ import Logo from "/src/assets/logo.svg";
 import "./Navbar.css"
 
 function NavBar() {
-    const [open, setOpen] = useState(false)
-    return (
-        <>
-    <header className="header">
-  <div className="header__logo">
-    <img src={Logo} alt="Logo" />
-  </div>
+  const [open, setOpen] = useState(false)
+  return (
+    <>
+      <header className="header">
+        <div className="header__logo">
+          <img src={Logo} alt="Logo" />
+        </div>
 
-  <nav className={`header__nav ${open ? "is-open" : ""}`}>
-    <ul>
-      <li><a href="#home">Home</a></li>
-      <li><a href="#learn">ABOUT US</a></li>
-      <li><a href="#tech">Our Services</a></li>
-      <li><a href="#contact">Contact</a></li>
-    </ul>
-  </nav>
+        <nav className={`header__nav ${open ? "is-open" : ""}`}>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#learn">ABOUT US</a></li>
+            <li><a href="#tech">Our Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+        <div className="header__cta">
+          <button
+            className="header__burger"
+            onClick={() => setOpen(!open)}
+            aria-label="Menu"
+            aria-expanded={open}
+          >
+            ☰
+          </button>
 
-  
-</header>
+        </div>
 
-        </>
-    )
+      </header>
+
+    </>
+  )
 }
 export default NavBar
